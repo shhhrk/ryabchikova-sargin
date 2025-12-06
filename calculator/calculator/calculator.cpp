5ae5f4ded777039be85411d4274266c99ba44163
Yana <zyf25092005@yandex.ru>
2025-12-06 04:22:20 +0300

    Добавлены операции и модернизирован код

--- calculator/calculator/calculator.cpp
+++ calculator/calculator/calculator.cpp
@@ -9,9 +9,14 @@ double subtract(double a, double b) {
     return a - b;
 }
 
+double multiply(double a, double b) {
+    return a * b;
+}
+
 void print_menu() {
     cout << "1. Сложение" << endl;
     cout << "2. Вычитание" << endl;
+    cout << "3. Умножение" << endl;
     cout << "0. Выход" << endl;
     cout << "Выберите операцию: ";
 }
@@ -25,17 +30,22 @@ int main() {
         print_menu();
         cin >> choice;
 
-        if (choice == 1 || choice == 2) {
+        if (choice >= 1 && choice <= 3) {
             cout << "Введите первое число: ";
             cin >> num1;
             cout << "Введите второе число: ";
             cin >> num2;
 
-            if (choice == 1) {
+            switch (choice) {
+            case 1:
                 result = sum(num1, num2);
-            }
-            else if (choice == 2) {
+                break;
+            case 2:
                 result = subtract(num1, num2);
+                break;
+            case 3:
+                result = multiply(num1, num2);
+                break;
             }
 
             cout << "Результат: " << result << endl;
